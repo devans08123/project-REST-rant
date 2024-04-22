31dbe10572ef992f6fa6fcc00f7026d96e1875c8
Donald Evans <devans08123@gmail.com>
2024-04-21 20:53:40 -0400

    addingh home page view

---
+++ views/home.jsx
@@ -0,0 +1,23 @@
+const React = require('react')
+const Def = require('./default')
+
+function home () {
+    return (
+        <Def>
+            <main>
+                <h1>REST-Rant</h1>
+                <div>
+                    <img src="/images/meat-&-veggies-bowl.jpg" alt="Bowl" />
+                    <div>
+                      Photo by <a href="https://unsplash.com/@pwign?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Anh Nguyen</a> on <a href="https://unsplash.com/images/food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                    </div>
+                </div>
+                <a href="/places">
+                    <button className="btn-primary">Places Page</button>
+                </a>
+            </main>
+        </Def>
+    )
+}
+
+module.exports = home
