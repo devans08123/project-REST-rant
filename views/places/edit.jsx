7e4943cbabac8f8f25b3a00cee7f69786ab7cf60
Donald Evans <devans08123@gmail.com>
2024-04-24 20:44:50 -0400

    adding edit function

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -1,19 +1,19 @@
 const React = require('react')
-const Def = require('../default.jsx')
+const Def = require('../default')
 
-function edit_form (data) {
+function edit_form(data) {
     return (
-        <Def>
-          <main>
-          <form method="POST" action={`/places/${data.id}?_method=PUT`}>
-                    <div className="row">
-                     < div className="form-group col-sm-6">
-                       <label htmlFor="name">Place Name</label>
-                     <input className="form-control" id="name" name="name" value={data.place.name} required />
-                    </div>
-          </main>
-        </Def>
-    )
-}
+       <Def>
+        <main>
+            <form method="POST" action={`/places/${data.id}?_method=PUT`}>
+                        <div className="row">
+                            <div className="form-group col-sm-6">
+                                <label htmlFor="name">Place Name</label>
+                                <input className="form-control" id="name" name="name" value={data.place.name} required />
+                            </div>
+                            <div className="form-group col-sm-6">
+                                <label htmlFor="pic">Place Picture</label>
+                                <input className="form-control" id="pic" name="pic" value={data.place.pic} />
+                            </div>
 
 module.exports = edit_form
