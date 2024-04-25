4996558a24393c376a94ecdbaa60e340d0748aca
Donald Evans <devans08123@gmail.com>
2024-04-24 20:31:29 -0400

    adding edit form

--- controllers/places.js
+++ controllers/places.js
@@ -9,6 +9,20 @@ router.post('/', (req, res) => {
     res.send('POST /places')
   })
   
+  router.get('/:id/edit', (req, res) => )
+    db.Place.findById(req.params.id)
+    .then(place => {
+        res.render('places/edit', { place })
+    })
+    router.get('/:id/edit', (req, res) => {
+      db.Place.findById(req.params.id)
+      .then(place => {
+          res.render('places/edit', { place })
+      })
+      .catch(err => {
+          res.render('error404')
+      })
+    
 
 router.get('/new', (req, res) => {
     res.render('places/new')
---
+++ views/places/edit.jsx
@@ -0,0 +1,19 @@
+const React = require('react')
+const Def = require('../default.jsx')
+
+function edit_form (data) {
+    return (
+        <Def>
+          <main>
+          <form method="POST" action={`/places/${data.id}?_method=PUT`}>
+                    <div className="row">
+                     < div className="form-group col-sm-6">
+                       <label htmlFor="name">Place Name</label>
+                     <input className="form-control" id="name" name="name" value={data.place.name} required />
+                    </div>
+          </main>
+        </Def>
+    )
+}
+
+module.exports = edit_form
